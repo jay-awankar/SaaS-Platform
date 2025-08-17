330e43739598c101690cba31544af594970ca912
jay-awankar <jatinawankar02@gmail.com>
2025-08-17 23:22:08 +0530

    Resolved build error of dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
-import {recentSessions} from "@/constants";
 import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
 import {getSubjectColor} from "@/lib/utils";
+
+export const dynamic = "force-dynamic";
 
 const Page = async () => {
     const companions = await getAllCompanions({ limit: 3 });
